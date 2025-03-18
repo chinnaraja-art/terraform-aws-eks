539841a89d99d0e0e220d5245c946c422e28b9fc
chinnaraja <chinnaraja118@gmail.com>
2025-03-18 22:25:17 +0530

    expense infra for k8s

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -5,14 +5,15 @@ terraform {
       version = "5.84.0"
     }
   }
-}
   backend "s3" {
     bucket = "expense-dev-82"
     key    = "expense-dev-eks-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "expense-dev82"
 
+  }
 }
+  
 
 provider "aws" {
   # Configuration options
